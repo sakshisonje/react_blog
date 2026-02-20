1ba33c335ff57247c5180b2410fb9ab4b46365be
null <null>
2026-02-20 15:42:37 +0530

    push 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
-import heroImg from './assets/hero.png';
-import './App.css';
+// import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <div>
       <h1>Hello React</h1>
-    </>
+      <h1>Hello React</h1>
+    </div>
+   
   );
 }
 
